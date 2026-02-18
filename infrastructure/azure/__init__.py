ef7e6449e0dfad2311df7fdb9d850f34b28d6078
petter2025us <petter2025us@outlook.com>
2026-02-18 09:54:03 -0500

    Create __init__.py

---
+++ infrastructure/azure/__init__.py
@@ -0,0 +1,5 @@
+"""Azure-specific infrastructure simulation components."""
+
+from agentic_reliability_framework.infrastructure.azure.azure_simulator import AzureInfrastructureSimulator
+
+__all__ = ["AzureInfrastructureSimulator"]
