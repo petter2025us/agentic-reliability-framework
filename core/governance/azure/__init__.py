f3565fd7005468d80ef8796be547018f99b65be0
petter2025us <petter2025us@outlook.com>
2026-02-28 16:56:54 +0000

    chore: remove old files and finalize v4 migration

--- core/governance/azure/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Azure-specific infrastructure simulation components."""
-
-from agentic_reliability_framework.infrastructure.azure.azure_simulator import AzureInfrastructureSimulator
-
-__all__ = ["AzureInfrastructureSimulator"]
